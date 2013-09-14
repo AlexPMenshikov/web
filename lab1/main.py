7815f4589c06bacb985f1948ce483b4b56d08463
Alexandr <Skimn1990@gmail.com>
2013-09-14 14:55:56 +0600

    add report, change port and number of client

--- lab1/main.py
+++ lab1/main.py
@@ -1,14 +1,13 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host = '0.0.0.0'
-port = 8007
+host = '127.0.0.1'
+port = 8000
 server_socket.bind((host,port))
-server_socket.listen(3)
+server_socket.listen(1)
 conn , addr = server_socket.accept()
 
 print 'client is at', addr
-#while 1:
 data = conn.recv(1024)
 c=conn.getpeername
 print(c)
