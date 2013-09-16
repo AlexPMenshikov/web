0e857da576b351fce6a2dc1502c30f3d32a32794
Alexandr <Skimn1990@gmail.com>
2013-09-16 18:56:59 +0600

    Lab2: unlucky

--- lab1/main.py
+++ lab1/main.py
@@ -5,8 +5,10 @@ host = '127.0.0.1'
 port = 8000
 server_socket.bind((host,port))
 server_socket.listen(1)
-conn , addr = server_socket.accept()
-print 'client is at', addr
-data = conn.recv(1024)
-conn.send(data)
+while 1:
+    conn , addr = server_socket.accept()
+    print 'client is at', addr
+    data = conn.recv(1024)
+    conn.send(data)
+    break
 conn.close()
--- lab2/server.py
+++ lab2/server.py
@@ -4,12 +4,20 @@ import os
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = '127.0.0.1'
 port = 8000
-path="C:/"
+path="D:/"
 server_socket.bind((host,port))
 server_socket.listen(1)
-conn , addr = server_socket.accept()
-data = os.listdir(path)+"\n"+conn.recv(1024)
-conn.send(data)
-#conn.sendall(data)
-#print(os.getcwd())
+while 1:
+    conn , addr = server_socket.accept()
+    print 'client is at', addr
+    tmp = os.listdir(path)
+    #ltmp = len(tmp)
+    data = ""
+    for x in tmp:
+        #data = data+x+"\n"
+        conn.send(x+"\n")
+    #data = conn.recv(1024)
+    #print(data)
+    #conn.send(data)
+    break
 conn.close()
