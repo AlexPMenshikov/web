a8882ac71589cd1ea2ae51b2c7e5b86ab38318a4
Alexandr <Skimn1990@gmail.com>
2013-09-16 18:30:36 +0600

    Lab2:start

---
+++ lab2/main.py
@@ -0,0 +1,5 @@
+import os
+path="C:/"
+p=os.listdir(path)
+print(p)
+print(os.getcwd()) #os.listdir(path))
--- lab2/server.py
+++ lab2/server.py
@@ -1,12 +1,15 @@
 import socket
+import os
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = '127.0.0.1'
 port = 8000
+path="C:/"
 server_socket.bind((host,port))
 server_socket.listen(1)
 conn , addr = server_socket.accept()
-print 'client is at', addr
-data = conn.recv(1024)
+data = os.listdir(path)+"\n"+conn.recv(1024)
 conn.send(data)
+#conn.sendall(data)
+#print(os.getcwd())
 conn.close()
